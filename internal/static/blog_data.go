38170fc9ed3cf709b54347f30922166927c43a61
robg-test <robertgaskin@hotmail.com>
2025-02-11 19:37:50 +0000

    Made meta data a global static object

---
+++ internal/static/blog_data.go
@@ -0,0 +1,38 @@
+package static
+
+import "github.com/robgtest/blog/internal/models"
+
+var ControlAndChoiceData = models.BlogMeta{
+	Title:       "Weekly Stoic: Control & Choice",
+	Url:         "https://blog.bob-productions.dev/blog/3",
+	Description: "Quick testing wellbeing tidbits",
+	ImageUri:    "https://blog.bob-productions.dev/images/stoic1.jpeg",
+}
+
+var ToBeSteadyData = models.BlogMeta{
+	Title:       "Weekly Stoic: To Be Steady & Unsteady",
+	Url:         "https://blog.bob-productions.dev/blog/5",
+	Description: "Quick testing wellbeing tidbits",
+	ImageUri:    "https://blog.bob-productions.dev/images/stoic2.jpg",
+}
+
+var AWSServerlessData = models.BlogMeta{
+	Description: "A Guide for performant cloud architecture: AWS Lambdas",
+	Url:         "https://blog.bob-productions.dev/blog/2",
+	Title:       "Software Performance Guide: AWS Lambdas",
+	ImageUri:    "https://blog.bob-productions.dev/images/AWS-Meta.png",
+}
+
+var IntroData = models.BlogMeta{
+	Description: "Who is the mysterious sweaty fox",
+	Url:         "https://blog.bob-productions.dev/blog/1",
+	Title:       "An Introduction To Bob Productions",
+	ImageUri:    "https://blog.bob-productions.dev/images/Sweat.webp",
+}
+
+var IsCopilotADudData = models.BlogMeta{
+	Description: "An opinion piece on what one could consider a disastrous implementation.",
+	Url:         "https://blog.bob-productions.dev/blog/2",
+	Title:       "The AI Auto-complete Headache",
+	ImageUri:    "https://blog.bob-productions.dev/images/copilot.webp",
+}
