567d0c24b1bd89e2d0982e2c0fd43f360229de1b
robg-test <robg-test@users.noreply.github.com>
2026-05-04 17:03:40 +0100

    Fix typo and remove unreachable code in database initialization

--- internal/database.go
+++ internal/database.go
@@ -12,10 +12,9 @@ var DB *sql.DB
 func InitDB(dbPath string) error {
 	var err error
 	DB, err = sql.Open("libsql", dbPath)
-	log.Print("Initalized Turso Database")
+	log.Print("Initialized Turso Database")
 	if err != nil {
 		log.Fatalf("Error opening database: %v", err)
-		return err
 	}
 	return nil
 }
